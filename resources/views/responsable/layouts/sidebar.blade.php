39d1eac3b4e8cff60285d3e983c637e75806a336
unknown <salamjeanlouis3@gmail.com>
2026-03-06 15:37:45 +0000

    fix: admin pour action

--- resources/views/responsable/layouts/sidebar.blade.php
+++ resources/views/responsable/layouts/sidebar.blade.php
@@ -192,14 +192,14 @@
             Tâches
         </a>
 
-        <a href="{{ route('responsable.daily-logs.create') }}"
+        <!-- <a href="{{ route('responsable.daily-logs.create') }}"
             class="nav-link {{ request()->routeIs('responsable.daily-logs.create') ? 'active' : '' }}">
             <svg width="17" height="17" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
                 <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
             </svg>
             Rédiger mon rapport
-        </a>
+        </a> -->
 
         <a href="{{ route('responsable.daily-logs.index') }}"
             class="nav-link {{ request()->routeIs('responsable.daily-logs.index') ? 'active' : '' }}">
